b344c22be56cad007c695dfec8a5a78ac646715a
hbkz0r <andythehellboy@gmail.com>
2017-03-31 02:57:31 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -6,11 +6,3 @@ Citizen.CreateThread(function()
 		end
 	end
 end)
-
-RegisterNetEvent("dropweapon")
-AddEventHandler('dropweapon', function()
-	local ped = GetPlayerPed(-1)
-	if DoesEntityExist(ped) and not IsEntityDead(ped) then
-		SetPedDropsWeapon(ped)
-	end
-end)
