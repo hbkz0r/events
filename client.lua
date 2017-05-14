13ccce96a0a6ce60f1d2e13eeb9bdd8ce23f3431
hbkz0r <andythehellboy@gmail.com>
2017-05-14 22:13:49 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,32 @@
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
+		if DoesEntityExist(GetPlayerPed(-1)) then
+			SetEntityInvincible(GetPlayerPed(-1), false)
+			SetPedCanBeTargetted(GetPlayerPed(-1), true)
+			SetPlayerWantedLevel(PlayerId(), 0, false)
+			SetPlayerWantedLevelNow(PlayerId(), false)
+			SetPlayerHealthRechargeMultiplier(PlayerId(), 0)
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
 		if IsPedBeingStunned(GetPlayerPed(-1)) then
-		ragdol = 1
+			ragdol = 1
+		end
+	end
+end)
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsEntityPlayingAnim(GetPlayerPed(-1), "dead", "dead_e", 3) then
+			SetPedCanRagdoll(GetPlayerPed(-1), false)
+			GivePlayerRagdollControl(PlayerId(), false)
 		end
 	end
 end)
