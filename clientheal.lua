e05d4660dcbf41a289eb515b39ffd698cacd9624
hbkz0r <andythehellboy@gmail.com>
2017-05-14 22:14:48 +0300

    Update clientheal.lua

--- clientheal.lua
+++ clientheal.lua
@@ -4,5 +4,22 @@ AddEventHandler('heal', function()
 	if DoesEntityExist(ped) and not IsEntityDead(ped) then
 		SetEntityHealth(ped, 200)
 		ragdol = 0
+		ShowNotification("You are getting healed.")
+
 	end
 end)
+
+RegisterNetEvent("stopanim")
+AddEventHandler('stopanim', function()
+	local ped = GetPlayerPed(-1)
+	if DoesEntityExist(ped) then
+	ClearPedTasksImmediately(ped)
+	end
+end)
+
+
+function ShowNotification(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(false, false)
+end
